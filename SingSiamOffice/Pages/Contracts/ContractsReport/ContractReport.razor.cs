1d6896465d6cbefe53907541b2d90b70dd13dae6
Kawank <kawan.k@transcodeglobal.com>
2024-08-27 17:55:12 +0700

    หน้าพิพม์สัญญา

---
+++ SingSiamOffice/Pages/Contracts/ContractsReport/ContractReport.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace SingSiamOffice.Pages.Contracts.ContractsReport
+{
+    public partial class ContractReport
+    {
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync("printReceipt");
+            }
+        }
+
+    }
+
+}
