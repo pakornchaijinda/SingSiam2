ce2cb0368fc854e3c9140b62a39dc4f429acd636
Kawank <kawan.k@transcodeglobal.com>
2024-11-05 11:33:11 +0700

    report portal

--- SingSiamOffice/Pages/Report/ReportPortal.razor.cs
+++ SingSiamOffice/Pages/Report/ReportPortal.razor.cs
@@ -1,6 +1,41 @@
-﻿namespace SingSiamOffice.Pages.Report
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SingSiamOffice.Pages.Report
 {
     public partial class ReportPortal
     {
+        [Inject]
+        NavigationManager navigation { get; set; }
+
+
+
+        private void bankreport()
+        {
+            navigation.NavigateTo("/bank-report");
+        }
+        private void branchexpensereport()
+        {
+            navigation.NavigateTo("/branchexpense-report");
+        }
+        private void conclusionreport()
+        {
+            navigation.NavigateTo("/conclusion-report");
+        }
+        private void expensesreport()
+        {
+            navigation.NavigateTo("/expenses-report");
+        }
+        private void moneycountreport()
+        {
+            navigation.NavigateTo("/moneycount-report");
+        }
+        private void montlyIncomereport()
+        {
+            navigation.NavigateTo("/montlyIncome-report");
+        }
+        private void transferreport()
+        {
+            navigation.NavigateTo("/transfer-report");
+        }
     }
 }
